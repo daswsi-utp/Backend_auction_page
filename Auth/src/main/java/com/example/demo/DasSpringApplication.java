90cd75ae3ed2c11772f17ecfa3ae0c68b24241ff
Sogni_Appesi <juegosxepor@gmail.com>
2025-05-04 16:52:52 -0500

    creation project

---
+++ Auth/src/main/java/com/example/demo/DasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasSpringApplication.class, args);
+	}
+
+}
