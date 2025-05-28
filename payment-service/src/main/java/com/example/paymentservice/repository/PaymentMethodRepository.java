64204429ebce9078f50d85306c19d693a5caf12e
DayanaCD <condoridaza26@gmail.com>
2025-05-28 00:12:47 -0500

    Implement PaymentMethodRepository with custom query and basic TransactionRepository

---
+++ payment-service/src/main/java/com/example/paymentservice/repository/PaymentMethodRepository.java
@@ -0,0 +1,10 @@
+package com.example.paymentservice.repository;
+
+import com.example.paymentservice.model.entity.PaymentMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
+    List<PaymentMethod> findByUserIdAndVisibleTrue(Long userId);
+}
